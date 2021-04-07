c490950fa4f3623a39e8ee3f490358805fc61aba
Teo Marcelo <tmarcelojr@gmail.com>
2021-04-07 13:09:42 -0500

    used beforeeach and assertThat in code

--- src/main/java/com/example/unitvsint/controllers/HelloController.java
+++ src/main/java/com/example/unitvsint/controllers/HelloController.java
@@ -2,7 +2,9 @@ package com.example.unitvsint.controllers;
 
 public class HelloController {
 
-    public String hello(String name) {
-        return String.format("Hello, %s!", name);
+    public String hello(String firstName, String lastName) {
+        // In Java, %s takes in any value which is then assigned by the following parameter.
+
+        return String.format("Hello, %s %s!", firstName, lastName);
     }
 }
--- src/test/java/com/example/unitvsint/controllers/HelloControllerTest.java
+++ src/test/java/com/example/unitvsint/controllers/HelloControllerTest.java
@@ -1,18 +1,28 @@
 package com.example.unitvsint.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class HelloControllerTest {
+    public HelloController controller;
+
+    // Before each is a void setup that runs before each test as a configuration
+    // Useful for avoiding instantiating classes for each test
+    @BeforeEach
+    void setUp() {
+        controller = new HelloController();
+    }
 
     @Test
     void hello() {
-    //    Purpose is to validate that each unit of the software is doing what it's supposed to do.
-    //    Create an instance of the class HelloController
-        HelloController controller = new HelloController(); // Arrange
-        String response = controller.hello("World"); // Act
+        // Purpose is to validate that each unit of the software is doing what it's supposed to do.
+        // Create an instance of the class HelloController. Since we have it in our BeforeEach we will no longer need to put it in here.
+        String response = controller.hello("Teo", "Marcelo"); // Act
         // Created an instance, passing in the argument for name
-        assertEquals("Hello, World!", response); // Assert
+        assertThat(response).isEqualTo("Hello, Teo Marcelo!"); // Assert
+        // You can write assertThat in this format for better readability or you can write it this way:
+        // assertEquals("Hello, World", response);
     }
 }
