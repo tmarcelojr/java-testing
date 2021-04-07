a3597dac885c7c0242c4268106673a0bed73288b
Teo Marcelo <tmarcelojr@gmail.com>
2021-04-07 12:31:48 -0500

    initialized project with spring

---
+++ src/main/java/com/example/unitvsint/UnitVsIntApplication.java
@@ -0,0 +1,13 @@
+package com.example.unitvsint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitVsIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitVsIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unitvsint/UnitVsIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unitvsint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitVsIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
